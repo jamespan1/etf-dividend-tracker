bd28d54b5e6aab094e3de27e180be08fd60ee31f
jamespan1 <107165516+jamespan1@users.noreply.github.com>
2026-09-17 20:46:40 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='etf-dividend-v1',FILES=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(FILES))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
